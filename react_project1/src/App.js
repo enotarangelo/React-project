cf6ba2d5e4991f0ab4bc4370f52d0ff11b797d1e
enotarangelo <93266827+enotarangelo@users.noreply.github.com>
2022-11-29 12:58:05 +0100

    update of 29/11/22
    componentDidMount + webpack.config.js, onSearchChange, updated render

--- react_project1/src/App.js
+++ react_project1/src/App.js
@@ -1,30 +1,38 @@
 import './App.css';
+import React from 'react';
 import {Component} from 'react'
-import { response } from 'express';
+
 
 class App extends Component {
   constructor(){
     super();
     this.state={
-      utenti:[]
+      searchField:"",
+      utenti:[],
     }
   }
-  
+
   componentDidMount(){
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(res => res.json())
     .then(dati => this.setState({utenti: dati}))
     .catch(err => console.error(err));
   }
-   
+  
+  onSearchChange=(event) => {
+    const searchField= event.target.value.toLowerCase();
+    this.setState(()=>{ return{searchField}});
+  }
+
   render(){
+    const {utenti, searchField}= this.state;
+    const {onSearchChange}= this
+    const filteredUtenti = utenti.filter((utenti)=>{return utenti.name.toLowerCase().includes(searchField)})  
+
     return (
       <div className="App">
-        <button onClick={()=>{
-          console.log('prova')
-        }
-        }>change name</button>
-        <p>{this.state.utenti.map((utenti)=><div key={utenti.id}><h1>{utenti.name}</h1></div>)}</p>
+        <input className='search-box' type='search' placeholder='search monster' onChange={onSearchChange}/>
+        {filteredUtenti.map((utente)=> <div key={utente.id}><h1>{utente.name}</h1></div>)}
       </div>
     );
   }
