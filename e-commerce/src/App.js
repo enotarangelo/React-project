5e3ff8ae2c95dc646f2f68bb3a48e5e2d1a00939
enotarangelo <93266827+enotarangelo@users.noreply.github.com>
2022-12-08 19:45:42 +0100

    update add login-in

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/router/home/home.compontent";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/router/navigation/navigation.compontent";
+import SignIn from "./components/router/sign-in/sign-in.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Shop= () => {
+  return ( 
+    <div>
+      <h1>Prova</h1>
     </div>
-  );
-}
+  )
+};
+
+
+const App = () => {
+ return (
+  <Routes>
+   <Route path="/" element={ <Navigation />} >
+    <Route index element={ <Home />} />
+    <Route path="shop" element={ <Shop />} />
+    <Route path="sign-in" element={ <SignIn />} />
+   </Route>
+  </Routes>
+
+ )
+};
 
 export default App;
---
+++ e-commerce/src/components/utils/firebase/firebase.utils.js
@@ -0,0 +1,56 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB2jJejeX9QdfELFz5_UOgZAgPs6ij2xeg",
+    authDomain: "e-commerce-271ee.firebaseapp.com",
+    projectId: "e-commerce-271ee",
+    storageBucket: "e-commerce-271ee.appspot.com",
+    messagingSenderId: "1017914451488",
+    appId: "1:1017914451488:web:0e14128d3b2a4d74d97e45"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const  googleProvider = new GoogleAuthProvider();
+  
+  googleProvider.setCustomParameters({
+    prompt: 'select_account',
+  });
+  
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth,  googleProvider);
+  
+  export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+  export const db = getFirestore();
+  
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+  
+    const userSnapshot = await getDoc(userDocRef);
+  
+    if (!userSnapshot.exists()) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+  
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt,
+        });
+      } catch (error) {
+        console.log('error creating the user', error.message);
+      }
+    }
+  
+    return userDocRef;
+  };
