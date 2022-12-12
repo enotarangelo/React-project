c76f919368a69174863dede7c30a06073641ad1b
enotarangelo <93266827+enotarangelo@users.noreply.github.com>
2022-12-13 00:01:48 +0100

    update to lesson 9
    sign-in, sign-up, form, authentication

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,28 +1,23 @@
-import Home from "./components/router/home/home.compontent";
-import { Routes, Route } from "react-router-dom";
-import Navigation from "./components/router/navigation/navigation.compontent";
-import SignIn from "./components/router/sign-in/sign-in.component";
+import { Routes, Route } from 'react-router-dom';
 
-const Shop= () => {
-  return ( 
-    <div>
-      <h1>Prova</h1>
-    </div>
-  )
+import Home from './router/home/home.compontent';
+import Navigation from './router/navigation/navigation.compontent';
+import Authentication from './router/authentication/authentication.component';
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
 };
 
-
 const App = () => {
- return (
-  <Routes>
-   <Route path="/" element={ <Navigation />} >
-    <Route index element={ <Home />} />
-    <Route path="shop" element={ <Shop />} />
-    <Route path="sign-in" element={ <SignIn />} />
-   </Route>
-  </Routes>
-
- )
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
